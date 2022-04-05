61828b5d6df455302b1b506cf8024f5a47c34229
Chris Ford <caf@slac.stanford.edu>
2022-04-05 09:45:46 -0700

    Use enableDrop directory.

--- enableDrop/enableDrop.sh
+++ enableDrop/enableDrop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source /cds/sw/ds/ana/conda2/manage/bin/psconda.sh
-# cd /reg/g/psdm/utils/enableDrop/
-cd /reg/g/psdm/utils/enableDrop.caf/
+cd /reg/g/psdm/utils/enableDrop/
 python enableDrop.py
