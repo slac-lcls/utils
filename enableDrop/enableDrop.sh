7856ea5acda56c523c70af50c392ffed28cc708c
Chris Ford <caf@slac.stanford.edu>
2022-04-04 18:28:12 -0700

    Rename drop directory to enableDrop.

--- enableDrop/enableDrop.sh
+++ enableDrop/enableDrop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /cds/sw/ds/ana/conda2/manage/bin/psconda.sh
-cd /reg/g/psdm/utils/enableDrop/
+# cd /reg/g/psdm/utils/enableDrop/
+cd /reg/g/psdm/utils/enableDrop.caf/
 python enableDrop.py
